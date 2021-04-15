13de9ff1419cdf818c8f7082eb8728fe79bdcf3d
Gabic <gabic@netrom.ro>
2021-04-15 11:58:38 +0300

    first & last commit boliler plated the code for later use

---
+++ Coding.Dona.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using System;
+
+namespace Coding.Dona.Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// The port number(5001) must match the port of the gRPC server.
+			using var channel = GrpcChannel.ForAddress("https://localhost:60387");
+			var client = new Greeter.GreeterClient(channel);
+			var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+			Console.WriteLine("Greeting: " + reply.Message);
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Coding.Dona
+
+### Description
+Boiler plate code for a quick (netcore) implementation of gRPC.
+
+### HTTP support
+Added support for sideby-side gRPC and HTTP calling
+
+### Swagger
+Added swagger support to enable easy-testing
+
+### run & test
+Server is docker enabled, so watch-out the port mappings. there's a client to do rpc calls, but there's also swagger for https calls
